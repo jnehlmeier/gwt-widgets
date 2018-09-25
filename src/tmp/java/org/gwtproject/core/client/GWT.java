45eed0fa97fa9dcd93eec90232b8985cba95a621
Jens Nehlmeier <jens.nehlmeier@gmail.com>
2018-09-25 23:57:34 +0200

    Temporary GWT class with some more methods

---
+++ src/tmp/java/org/gwtproject/core/client/GWT.java
@@ -0,0 +1,69 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+package org.gwtproject.core.client;
+
+import elemental2.dom.DomGlobal;
+import elemental2.dom.DomGlobal.SetTimeoutCallbackFn;
+import jsinterop.annotations.JsMethod;
+
+public final class GWT {
+  public GWT() {
+  }
+
+  /** @deprecated */
+  @Deprecated
+  public static <T> T create(Class<?> clazz) {
+    return null;
+  }
+
+  /** @deprecated */
+  @Deprecated
+  public static boolean isProdMode() {
+    return !"on".equals(System.getProperty("superdevmode"));
+  }
+
+  /** @deprecated */
+  @Deprecated
+  public static String getModuleName() {
+    return null;
+  }
+
+  /** @deprecated */
+  @Deprecated
+  public static String getModuleBaseURL() {
+    return null;
+  }
+
+  /** @deprecated */
+  @Deprecated
+  public static void log(String msg) {
+
+  }
+
+  /** @deprecated */
+  @Deprecated
+  public static void log(String msg, Exception e) {
+
+  }
+
+  /** @deprecated */
+  @Deprecated
+  public static void reportUncaughtException(Throwable e) {
+    DomGlobal.setTimeout((ignore) -> {
+      throw_(e);
+    }, 0.0D, new Object[0]);
+  }
+
+  @JsMethod(
+      namespace = "<window>",
+      name = "throw"
+  )
+  private static native void throw_(Object var0);
+
+  public static boolean isScript() {
+    return true;
+  }
+}
