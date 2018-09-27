002480995144aa75216f6b4e9c51880cb00e6563
Jens Nehlmeier <jens.nehlmeier@gmail.com>
2018-09-27 22:19:06 +0200

    Use old GWT class because FormPanel requires GWT.getModuleName()

--- src/main/java/org/gwtproject/user/client/ui/FormPanel.java
+++ src/main/java/org/gwtproject/user/client/ui/FormPanel.java
@@ -15,7 +15,7 @@
  */
 package org.gwtproject.user.client.ui;
 
-import org.gwtproject.core.client.GWT;
+import com.google.gwt.core.client.GWT;
 import org.gwtproject.core.client.Scheduler;
 import org.gwtproject.core.client.Scheduler.ScheduledCommand;
 import org.gwtproject.dom.client.Document;
